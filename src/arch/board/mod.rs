32e3b868e441bd317736938a69aa0a8e11ac0398
Christian Ehrig <cehrig@cloudflare.com>
2024-10-15 01:19:26 +0200

    cfg_attr

--- src/arch/board/mod.rs
+++ src/arch/board/mod.rs
@@ -1,11 +1,5 @@
-#[cfg(feature = "st32f4xx")]
-mod st32f4xx;
+#[cfg_attr(all(target_arch = "arm", target_abi = "eabihf"), path = "st32f4xx.rs")]
+#[cfg_attr(all(target_arch = "arm", target_abi = "eabi"), path = "st32f7xx.rs")]
+mod _target;
 
-#[cfg(feature = "st32f4xx")]
-pub use st32f4xx::*;
-
-#[cfg(feature = "st32f7xx")]
-mod st32f7xx;
-
-#[cfg(feature = "st32f7xx")]
-pub use st32f7xx::*;
+pub use _target::*;
