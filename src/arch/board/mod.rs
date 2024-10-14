e047668a01e5876c3d711b4db819abc6dc9e55f6
Christian Ehrig <cehrig@cloudflare.com>
2024-10-15 00:26:20 +0200

    Remove LD_STACK_PTR from board module

--- src/arch/board/mod.rs
+++ src/arch/board/mod.rs
@@ -1,15 +1,11 @@
 #[cfg(feature = "st32f4xx")]
-pub mod st32f4xx;
+mod st32f4xx;
 
 #[cfg(feature = "st32f4xx")]
 pub use st32f4xx::*;
 
 #[cfg(feature = "st32f7xx")]
-pub mod st32f7xx;
+mod st32f7xx;
 
 #[cfg(feature = "st32f7xx")]
 pub use st32f7xx::*;
-
-extern "C" {
-    pub static LD_STACK_PTR: usize;
-}
